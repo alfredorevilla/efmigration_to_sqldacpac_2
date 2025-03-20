0930fdf80a23e4b63710312f4f0da928208c3acb
Alfredo Revilla <alfredorevilla@gmail.com>
2025-03-20 09:53:08 -0500

    Add VSCode settings and PowerShell script for DACPAC extraction

---
+++ Add-DacPac.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [string]
+    # [parameter(mandatory = $true)]
+    $SourceConnectionString = "Server=(localdb)\mssqllocaldb; Database=efmigration_to_sqldacpac_2_publish",
+    [parameter(Mandatory = $true)]
+    [String]
+    $MigrationName
+)
+
+Start-Process sqlpackage.exe -ArgumentList "/a:Extract /SourceConnectionString:""$SourceConnectionString"" /TargetFile:$MigrationName.dacpac" -NoNewWindow -Wait
+
